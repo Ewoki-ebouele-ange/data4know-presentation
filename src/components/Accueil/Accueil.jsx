22f8a8c5a7aad51f96ccd0ccbfeda8abfa2afe1c
Angel-theblackboss <bntboris8@gmail.com>
2023-10-06 03:40:37 +0100

    Commit 06-08

--- src/components/Accueil/Accueil.jsx
+++ src/components/Accueil/Accueil.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Accueil.css'
 import data4 from "../../Assets/low logo blue.png"
 import Img from "../../Assets/low logo blue.png"
+import Sergio from '../../Assets/sergio.png'
 
 export default function Accueil(){
 
@@ -11,81 +12,89 @@ export default function Accueil(){
             <div className="location_meuble">
                 <div className="location_desc">
                     <h1 className="frigorifiques">
-                        Location de meubles frigorifiques
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit
                     </h1>
                     <p className="large">
-                        Un large choix de location de matériels réfrigérés récents disponibles en froid positif, en froid négatif ou en bi-température. <br /> <br />
-                        <a href="#">Bacs réfrigérés</a>, <a href="#">vitrines murales frigorifiques</a>, vitrines service arrière accompagnées de <a href="#">balances</a> et trancheurs spécialement conçus pour recevoir votre PLV afin de valoriser vos produits. <br /> <br />
-                        Notre métier : pourvoir à vos besoins ponctuels en froid. Notre force : la réactivité. France Gel est à votre écoute !
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sequi voluptate inventore exercitationem sint voluptatibus labore officiis molestiae tempora expedita rem quas eveniet, eos alias quia assumenda. Culpa, rem quo? <br /> <br />
+                        <a href="#">sit amet consectetur </a>, <a href="#">Beatae sequi voluptate inventore</a>, vitrines service arrière accompagnées de <a href="#">exercitationem</a> et trancheurs spécialement conçus pour recevoir votre PLV afin de valoriser vos produits. <br /> <br />
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem ad quod sed ab iusto, maiores deleniti magni. Quis, aliquam numquam?
                     </p>
-                    <span className="dem_devis">DEMANDE DE DEVIS EXPRESS</span>
+                    <span className="dem_devis">SKDYFIEFE LIZOFKZE LZIEHFZE</span>
                 </div>
                 <div className="image_acc">
                     <img src={data4} alt="image_face" />
                 </div>
             </div>
             <div className="decouverte">
-                <h2>Découvrez notre gamme de meubles réfrigérés à louer</h2>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing eli</h2>
                 <div className="vitrines">
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                     <div className="card_vit">
                             <img src={Img} alt="" />
                         <div className="desc_img">
-                            <h2>Vitrines verticales réfrigérées</h2>
-                            <p>Un large choix de vitrines verticales réfrigérées de toutes les dimensions</p>
+                            <h2>consectetur adipisicing elit</h2>
+                            <p>Officia ullam inventore voluptates temporibus recusandae dolore dignissimos tempore quis autem</p>
                         </div>
                     </div>
                 </div>
             </div>
-            
+            <div className="my_present">
+                <img src={Sergio} alt="profil_image" />
+                <div className="my_desc">
+                    <h1>KSDJ KUECFZEF LOCDUIUFSD KIUHDF</h1>
+                    <h2>SDKSD OMOLMFV LVKLIPOQ EZCYSDYC HYGSDYFCSD YTFCTDQ QYTFSQ</h2>
+                    <span className="hr1"></span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam accusantium recusandae voluptate modi impedit similique dolor? A neque</p>
+                </div>
+            </div>
         </div>
     )
 }
