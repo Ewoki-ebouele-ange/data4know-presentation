3cd8849a5ae48e023e9e237e1fdf1f1b61614010
seewe <40412177+seewe@users.noreply.github.com>
2023-10-23 23:04:26 -0400

    Update Footer.jsx
    contact updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,23 +8,23 @@ export default function Footer(){
                 <div className="site_information">
                     <span className="tit">DATA4KNOW</span>
                     <span>
-                    <a href="#">Rue Babelogne </a><br />
-                    <a href="#">80800 Corbie</a>
+                    <a href="#">Canada, Québec</a><br />
+                    <a href="#">Capitale Nationale, Beauport</a>
                     </span>
-                    <span>mail : <a href="#">location@francegel.com</a></span>
-                    <span>Tél : <a href="#">03.22.43.87.94</a></span>
+                    <span>mail : <a href="#">info@dataforknow.com</a></span>
+                    <span>Tél : <a href="#">581 0000 0000</a></span>
                 </div>
                 <div className="liens_utiles">
                     <span className="tit">LIENS UTILES</span>
                     <span>
-                    <a href="#">Louer un produit </a><br />
-                    <a href="#"> Acheter un produit </a> <br />
-                    <a href="#">Nous contacter</a>
+                    <a href="#"> Facebook </a><br />
+                    <a href="https://www.youtube.com/channel/UCyGGK5ScFKt4IS7yHQ0Tmzw"> Notre chaine YouTube </a> <br />
+                    <a href="#">LinkedIn</a>
                     </span>
                 </div>
                 <div className="ment_leg">
                    <span> Mentions légales</span>
-                   <span>Création de site : <a href="#">Ewoki Ebouele</a></span>
+                   <span>Création de site : <a href="#">dataforknow(Avalange)</a></span>
                 </div>
         </div>    
     )
